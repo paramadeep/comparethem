59acc2bf3baaaaf5e111bfb58c98ac29d5424902
deepak <paramadeep@users.noreply.github.com>
2022-12-01 12:51:13 +0530

    Create domain.js

---
+++ domain.js
@@ -0,0 +1,46 @@
+export default {
+  name: "ice cream bill",
+  defaults: ["quantity", "coating", "finish"],
+  customTopFields: [
+    { name: "Quotation For", value: "" },
+    { name: "Quotation Date", value: new Date().toLocaleDateString() },
+  ],
+  customBottomFields: [{ name: "Delivery Date", value: "" }],
+  blocks: [
+    { name: "quantity", input: 1 },
+    {
+      name: "coating",
+      input: ["varnish", "aqua"],
+      compute: (i, o) => {
+        const costMap = { varnish: 3, aqua: 2 };
+        o.cost = o.cost + costMap[i.coating];
+      },
+    },
+    {
+      name: "finish",
+      input: ["matt", "gloss"],
+      compute: (i, o) => {
+        const costMap = { matt: 4, gloss: 5 };
+        o.cost = o.cost + costMap[i.finish];
+      },
+    },
+    {
+      name: "colors",
+      input: ["four", "double", "single"],
+      compute: (i, o) => {
+        const costMap = { four: 3, double: 2, single: 1 };
+        o.cost = o.cost + costMap[i.colors] * i.quantity;
+      },
+    },
+    {
+      name: "side",
+      input: 2,
+    },
+  ],
+  output: ["cost"],
+  reportRow: "quantity",
+  reportValue: "cost",
+  reportGroup: ["coating"],
+  reportSpecs: ["side"],
+  customFields: ["customer name"],
+}
